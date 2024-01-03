16763c53347e2f9a908d39100a0a71d96d911168
kkkkcE <839043073@qq.com>
2024-01-03 21:54:25 +0800

    Site updated: 2024-01-03 21:54:23

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/12/28/hello-world/","2024/01/03/video-test1-backup/","2024/01/02/input-test1/","2023/12/29/video-test1/"];function toRandomPost(){
+var posts=["2023/12/28/hello-world/","2024/01/02/input-test1/","2024/01/03/video-test1-backup/","2023/12/29/video-test1/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
