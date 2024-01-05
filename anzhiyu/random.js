efce9518b39bf58a1231bad604a78cd11a06c882
kkkkcE <839043073@qq.com>
2024-01-05 16:20:14 +0800

    Site updated: 2024-01-05 16:20:11

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/01/02/input-test1/","2024/01/03/video-test1-backup/","2023/12/29/video-test1/","2024/01/04/文章进html/","2023/12/28/hello-world/"];function toRandomPost(){
+var posts=["2024/01/02/input-test1/","2023/12/28/hello-world/","2024/01/05/一些代码记录/","2024/01/03/video-test1-backup/","2023/12/29/我让你看meme你聋吗/","2024/01/04/文章进html/","2024/01/05/第一篇DIY文章/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
